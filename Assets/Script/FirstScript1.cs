3bfd13bf39cac2da1fb18be419a2b6116360d92e
MSANTOS\gmosa <gmosantos@outlook.com>
2025-01-15 18:43:48 -0500

    Some Practice

--- Assets/Script/FirstScript1.cs
+++ Assets/Script/FirstScript1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FirstScript1 : MonoBehaviour
 {
-    float speed = 1f;
+    public float speed = 0.05f;
 
 
     // Start is called before the first frame update
@@ -18,12 +18,20 @@ public class FirstScript1 : MonoBehaviour
     {
 
         Vector3 pos = transform.position;
-        pos.x += speed;
+        pos.x += speed * Time.deltaTime; // Time.deltaTime Changes it MPS/UnitsPS
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
 
-        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        if (screenPos.x < 0 )
         {
+            Vector3 fixedPos = new Vector3(0,0,0);
+           pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speed = speed * -1;
+        }
+        if (screenPos.x > Screen.width)
+        {
+            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
             speed = speed * -1;
         }
         transform.position = pos;
